459cd252a6a11ad7935435d9793ae0e4b0c60472
Russell <russell.saw@oriental.com.au>
2025-07-15 16:49:18 +1000

    landing page ui tweaks and spacing

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -4,7 +4,7 @@ import ProjectThree from './projects/ProjectThree'
 
 export default function ProjectsSection() {
   return (
-    <section style={{ width: '100%', gap: 50, display: 'flex', flexDirection: 'column', paddingBottom: 150 }}>
+    <section style={{ width: '100%', gap: 50, display: 'flex', flexDirection: 'column', paddingBottom: 150, paddingTop: 150}}>
       <ProjectOne />
       <ProjectTwo  />
       <ProjectThree  />
